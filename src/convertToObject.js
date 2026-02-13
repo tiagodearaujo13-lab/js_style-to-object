4570f2d2795a116638e1717232d898a3b9f25218
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-13 10:28:14 +0000

    task done javascript advanced to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,24 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const cssDeclarations = sourceString
+    .trim()
+    .split(';')
+    .map((declaration) => declaration.trim());
+
+  const result = {};
+
+  for (const declaration of cssDeclarations) {
+    if (declaration) {
+      const [key, value] = declaration.split(':');
+
+      if (key && value) {
+        result[key.trim()] = value.trim();
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
